67f0ae955f795dfbc8bb9adc2e73055df249ce29
Chinseok Lee <me@askcompany.kr>
2020-02-23 18:28:31 +0900

    TemplateHTMLRender APIView 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,7 @@ router.register('post', views.PostViewSet)  # 2개 URL을 만들어줍니다.
 # router.urls  # url pattern list
 
 urlpatterns = [
+    path('mypost/<int:pk>/', views.PostDetailAPIView.as_view()),
     # path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.decorators import api_view, action
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -62,3 +64,14 @@ class PostViewSet(ModelViewSet):
 #     # request.method # => 3개 분기
 #     pass
 
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'instagram/post_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        return Response({
+            'post': post,
+        })
