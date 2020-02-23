5e9cdc432885222c13a4f238991914cb8c6fcb77
Chinseok Lee <me@askcompany.kr>
2020-02-23 22:11:56 +0900

    accounts 앱 생성 및 등록

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('instagram.urls')),
 ]
