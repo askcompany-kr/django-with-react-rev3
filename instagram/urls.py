7541011c9d4323e8d298923826cb042a9b61726d
Chinseok Lee <me@askcompany.kr>
2020-02-23 17:33:13 +0900

    ModelViewSet에 커스텀 Endpoint 추가하기

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,6 @@ router.register('post', views.PostViewSet)  # 2개 URL을 만들어줍니다.
 # router.urls  # url pattern list
 
 urlpatterns = [
-    path('public/', views.public_post_list),
+    # path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -21,16 +21,30 @@ from .models import Post
 # public_post_list = PublicPostListAPIView.as_view()
 
 
-@api_view(['GET'])
-def public_post_list(request):
-    qs = Post.objects.filter(is_public=True)
-    serializer = PostSerializer(qs, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def public_post_list(request):
+#     qs = Post.objects.filter(is_public=True)
+#     serializer = PostSerializer(qs, many=True)
+#     return Response(serializer.data)
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        qs = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=['is_public'])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     # def dispatch(self, request, *args, **kwargs):
     #     print("request.body :", request.body)  # print 비추천, logger 추천
