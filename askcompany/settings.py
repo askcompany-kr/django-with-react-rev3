c86b55b959c30561eb0a12ac29ffaa0088742090
Chinseok Lee <me@askcompany.kr>
2020-02-23 22:01:23 +0900

    settings에 Throttle 샘플 설정 추가 및 주석 처리

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -128,4 +128,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.UserRateThrottle',
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     # 'anon': None,
+    #     'user': '3/day',
+    # },
 }
